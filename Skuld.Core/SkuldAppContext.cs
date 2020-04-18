406e90bfeee89ee59a9e68fde10d4fe53306a1d8
exsersewo <exsersewo@systemexit.co.uk>
2020-04-18 19:27:43 +0100

    Add Regex for weblinks

--- Skuld.Core/SkuldAppContext.cs
+++ Skuld.Core/SkuldAppContext.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace Skuld.Core
 {
@@ -21,6 +22,7 @@ namespace Skuld.Core
         public const string ConfigEnvVar = "SKULD_CONFIGID";
         public const string ConStrEnvVar = "SKULD_CONNSTR";
         public const string LogLvlEnvVar = "SKULD_LOGLEVEL";
+        public static Regex LinkRegex = new Regex(@"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)", RegexOptions.IgnoreCase);
 
         public static string GetCaller([CallerMemberName] string caller = null)
             => caller;
